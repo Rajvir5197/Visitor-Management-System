f19688283b2b1ae32948c33c6141adf20c8800c2
Prachi-9 <prachicanti@gmail.com>
2020-05-17 02:16:00 +0530

    done

--- target/visitor-Management-System/controller/securityController.js
+++ target/visitor-Management-System/controller/securityController.js
@@ -66,12 +66,25 @@ app.controller('securityController', function($scope, $rootScope, $http) {
 		};
 	};
 	
+	
+	
 	$scope.getCovisitorAsset = function(selectedCoVisitor){
 		$scope.selectedCoVisitor = selectedCoVisitor;
 		$http.post("/visitor-Management-System/Security/getAllAsset", selectedCoVisitor).then(function mySuccess(response){
 			$scope.allAsset = response.data;
 			$scope.newAsset={};
 			//$('#viewAssetModal').show();
+		}, function myError(data){
+			console.log("some internal error");
+			console.log(data);
+		});
+	}
+	
+	$scope.getVisitorAsset = function(){
+		$http.post("/visitor-Management-System/Security/getVisitAllAsset", $scope.visitCheckin.meetingBooked.visitor).then(function mySuccess(response){
+			$scope.allAsset = response.data;
+			$scope.newVisitorAsset={};
+			$('#viewVisitorAssetModal').modal('show');
 		}, function myError(data){
 			console.log("some internal error");
 			console.log(data);
@@ -92,12 +105,39 @@ app.controller('securityController', function($scope, $rootScope, $http) {
 		});
 	};
 	
+	$scope.addVisitorAsset = function(){
+		$scope.newVisitorAsset.assetStatus = "Not Delivered";
+		$scope.newVisitorAsset.deliveredFlag = false;
+		$scope.newVisitorAsset.mainVisitor = $scope.visitCheckin.meetingBooked.visitor;
+		$http.post("/visitor-Management-System/Security/addCoVisitorAsset", $scope.newVisitorAsset).then(function mySuccess(response){
+			if(response.data.msg == "SUCCESS"){
+				$scope.getVisitorAsset($scope.visitCheckin);
+			}
+		}, function myError(data){
+			console.log("some internal error");
+			console.log(data);
+		});
+	};
+	
 	$scope.deliverAsset = function(asset){
 		asset.assetStatus = "Delivered";
 		asset.deliveredFlag = true;
 		$http.post("/visitor-Management-System/Security/addCoVisitorAsset", asset).then(function mySuccess(response){
 			if(response.data.msg == "SUCCESS"){
 				$scope.getCovisitorAsset($scope.selectedCoVisitor);
+			}
+		}, function myError(data){
+			console.log("some internal error");
+			console.log(data);
+		});
+	};
+	
+	$scope.deliverVisitorAsset = function(asset){
+		asset.assetStatus = "Delivered";
+		asset.deliveredFlag = true;
+		$http.post("/visitor-Management-System/Security/addCoVisitorAsset", asset).then(function mySuccess(response){
+			if(response.data.msg == "SUCCESS"){
+				$scope.getVisitorAsset($scope.selectedCoVisitor);
 			}
 		}, function myError(data){
 			console.log("some internal error");
